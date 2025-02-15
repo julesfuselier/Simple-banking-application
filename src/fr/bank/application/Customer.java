d9addf57133355891b703e14644c42d7c3735fdb
Jules FUSELIER <jules.fuselier@etu.univ-amu.fr>
2025-02-15 17:04:49 +0100

    Modifications

--- src/fr/bank/application/Customer.java
+++ src/fr/bank/application/Customer.java
@@ -1,5 +1,66 @@
 package fr.bank.application;
+import java.time.LocalDate;
 
 public class Customer {
 
+    private String name;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String address;
+    private String city;
+    private LocalDate dateOfBirth;
+
+    public Customer(String name, String lastName, String email, String phone, String address, String city, LocalDate dateOfBirth) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.city = city;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
 }
