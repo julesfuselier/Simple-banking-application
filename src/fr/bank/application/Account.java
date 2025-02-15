e3de5d7cbbcbff9ab84ab9d71e9013d7c20e7da3
Dinesh <dinesh.radjou@laposte.net>
2025-02-15 17:21:26 +0100

    update Account class : name and toString

--- src/fr/bank/application/Account.java
+++ src/fr/bank/application/Account.java
@@ -5,22 +5,31 @@ import java.time.LocalDate;
 public class Account extends Customer{
     private int balance = 0;
     private String ID;
-    private String mdp;
+    private String Password;
 
-    public Account(String name, String lastName, String email, String phone, String address, String city, LocalDate dateOfBirth, String ID, String mdp) {
+    public Account(String name, String lastName, String email, String phone, String address, String city, LocalDate dateOfBirth, String ID, String Password) {
         super(name, lastName, email, phone, address, city, dateOfBirth);
         this.ID = ID;
-        this.mdp = mdp;
+        this.Password = Password;
     }
 
-
+    //getter and setter for : Balance
     public int getBalance() {return balance;}
     public void setBalance(int balance) {this.balance = balance;}
 
+    //getter and setter for : ID
     public String getID() {return ID;}
     public void setID(String ID) {this.ID = ID;}
 
-    public String getMdp() {return mdp;}
-    public void setMdp(String mdp) {this.mdp = mdp;}
+    //getter and setter for : Password
+    public String getPassword() {return Password;}
+    public void setPassword(String password) {this.Password = password;}
+
+    //METHODS
+
+
+    //toString
+    @Override
+    public String toString() {return "Account[" + super.toString() + "balance=" + balance + ", ID=" + ID + ", Password=" + Password;}
 
 }
